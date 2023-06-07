066ac64ff572b3b1e0ee3ab828b87be753d369f3
Nate-jon <4200172@schoolsnet.act.edu.au>
2023-06-07 14:32:26 +1000

    Update SmartDevice.ino

--- SmartDevice/SmartDevice.ino
+++ SmartDevice/SmartDevice.ino
@@ -8,22 +8,56 @@ RTC_Millis rtc;     // Software Real Time Clock (RTC)
 DateTime rightNow;  // used to store the current time.
 
 // SD Card - Confirm Pin
-#define SDpin 53
+#define SDPIN 53
+
+//trafic lights
+#define LEDRED A2
+#define LEDYELLOW A1 
+#define LEDGREEN A0
+
+//DC motor
+#include <L298N.h>
+
+// Pin definition
+const unsigned int IN1 = 7;
+const unsigned int IN2 = 8;
+const unsigned int EN = 9;
+
+// Create one motor instance
+L298N motor(EN, IN1, IN2);
+
+// Servo
+#include <Servo.h>
+Servo myservo;
+
+//Potentiometer
+#define POT A3
+
+// Piezo Buzzer
+#define piezoPin 5
+
+// Sonar - HC-SR04
+#define echoPin A4 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 53 //attach pin D3 Arduino to pin Trig of HC-SR04
+
+// Line Sensor
+#define lineSensorPin 3
+
+// Crash Sensor / Button
+#define crashSensor 7
+
+// RFID Start
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN  21  // ES32 Feather
+#define RST_PIN 17 // esp32 Feather - SCL pin. Could be others.
+
+MFRC522 rfid(SS_PIN, RST_PIN);
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);  // Open serial communications and wait for port to open:
-  while (!Serial) {
-    delay(1);  // wait for serial port to connect. Needed for native USB port only
-  }
-
-  // SD Card initialisation
-  Serial.print("Initializing SD card...");
-  if (!SD.begin(SDpin)) {
-    Serial.println("initialization failed!");
-    while (1)
-      ;
-  }
+  Serial.begin(9600); 
 }
 
 void loop() {
@@ -37,6 +71,10 @@ void loop() {
   delay(250);
   determineIfNight();
   delay(250);
+}
+
+void logEvent(string dataToLog){
+  Date
 }
 /*
  will determin if the crash button has been presed
